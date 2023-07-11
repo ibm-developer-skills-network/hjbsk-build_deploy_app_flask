e3901ab7a2b34b05da9108f9a9f6f9b8b9653d45
AbhishekGag <137916055+AbhishekGag@users.noreply.github.com>
2023-07-11 12:47:55 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+# Import the Maths package here
+
+app = Flask("Mathematics Problem Solver")
+
+@app.route("/sum")
+def sum_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    # Write your code here
+
+@app.route("/sub")
+def sub_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    # Write your code here
+
+@app.route("/mul")
+def mul_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    # Write your code here  
+
+@app.route("/")
+def render_index_page():
+    # Write your code here
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
