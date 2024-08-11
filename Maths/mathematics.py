7b70f8a739c0394cf6874e7fda5c9367bfc1324d
joeyceni <cenicola74@gmail.com>
2024-08-11 06:58:55 -0400

    update route methods

---
+++ Maths/mathematics.py
@@ -0,0 +1,11 @@
+def summation(a, b):
+    result = a + b
+    return result
+
+def subtraction(a, b):
+    result = a - b
+    return res
+
+def multiplication(a, b):
+    result = a * b
+    return res
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from Maths.mathematics import summation, subtraction, multiplication
 # Import the Maths package here
 
 app = Flask("Mathematics Problem Solver")
@@ -7,23 +8,26 @@ app = Flask("Mathematics Problem Solver")
 def sum_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here
+    result = summation(num1, num2)
+    return str(result)
 
 @app.route("/sub")
 def sub_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here
+    result = subtraction(num1, num2)
+    return str(result)
 
 @app.route("/mul")
 def mul_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here  
+    result = multiplication(num1, num2)
+    return str(result)
 
 @app.route("/")
 def render_index_page():
-    # Write your code here
+    return render_template('index.html')
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
