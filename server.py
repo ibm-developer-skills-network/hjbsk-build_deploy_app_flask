d6844641d835bb4087f02ef998f1dc5c00abd14c
Yaakov Yosef Miller <17897299+y44k0v@users.noreply.github.com>
2025-01-23 00:14:10 -0500

    Works without JavaScript

--- server.py
+++ server.py
@@ -1,29 +1,44 @@
 from flask import Flask, render_template, request
-# Import the Maths package here
+from Maths.mathematics import summation, substraction, multiplication
+
 
 app = Flask("Mathematics Problem Solver")
 
-@app.route("/sum")
+@app.route("/sum", methods=['POST'])
 def sum_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here
+    num1 = float(request.form.get('num1'))
+    num2 = float(request.form.get('num2'))
+    return str(summation(num1, num2))
 
-@app.route("/sub")
+@app.route("/sub", methods=['POST'])
 def sub_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here
+    num1 = float(request.form.get('num1'))
+    num2 = float(request.form.get('num2'))
+    return str(substraction(num1, num2))
 
-@app.route("/mul")
+@app.route("/mul", methods=['POST'])
 def mul_route():
-    num1 = float(request.args.get('num1'))
-    num2 = float(request.args.get('num2'))
-    # Write your code here  
+    num1 = float(request.form.get('num1'))
+    num2 = float(request.form.get('num2'))
+    return str(multiplication(num1, num2))
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def render_index_page():
-    # Write your code here
+    action = None
+    num1 = float(request.form.get('num1'))
+    num2 = float(request.form.get('num2'))
+    if 'add' in request.form:
+        action = sum_route()
+    elif 'sub' in request.form:
+        action = sub_route()
+        
+    elif 'mul' in request.form:
+        action = mul_route()
+        
+    else:
+        pass
+    
+    return render_template('index.html',num1=num1, num2=num2, action=action)
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8083, debug=True)
